11370890e7744c047b0d8e3be153fd84011d0bf7
alvar27 <faisalalvarisi027@gmail.com>
2025-08-16 11:44:08 +0700

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -12,7 +12,6 @@ function encrypt_url($data) {
     return str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($iv . $ciphertext . $hmac));
 }
 
-
 function decrypt_url($data) {
     $key = SECRET_KEY;
     $missing_padding = 4 - (strlen($data) % 4);
@@ -27,4 +26,59 @@ function decrypt_url($data) {
     $plaintext = openssl_decrypt($ciphertext, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
     if ($plaintext === false) return false;
     return $plaintext;
+}
+
+function detectUrl() {
+    $scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+    $domain = $scheme . "://" . $_SERVER['HTTP_HOST'] . "/";
+
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '';
+    $segments = $path === '' ? [] : explode('/', trim($path, '/'));
+
+    if (count($segments) === 1 && $segments[0] !== '') {
+        return [
+            'mode'   => 'full',
+            'domain' => $domain,
+            'page'   => $segments[0]
+        ];
+    } elseif (count($segments) > 1) {
+        $data = array_pop($segments);
+        return [
+            'mode'   => 'partial',
+            'domain' => $domain,
+            'path'   => implode('/', $segments) . '/',
+            'data'   => $data
+        ];
+    }
+
+    return [
+        'mode'   => 'empty',
+        'domain' => $domain
+    ];
+}
+
+function is_ciphertext($value) {
+    $result = decrypt_url($value);
+    return $result !== false;
+}
+
+function update_url() {
+    $urlMode = detectUrl();
+    $newUrl = null;
+
+    if ($urlMode['mode'] === 'full') {
+        if (is_ciphertext($urlMode['page'])) {
+            $page = decrypt_url($urlMode['page']);
+            $newUrl = $urlMode['domain'] . encrypt_url($page);
+        }
+    } elseif ($urlMode['mode'] === 'partial') {
+        if (is_ciphertext($urlMode['data'])) {
+            $data = decrypt_url($urlMode['data']);
+            $newUrl = $urlMode['domain'] . $urlMode['path'] . encrypt_url($data);
+        }
+    }
+
+    if ($newUrl !== null) {
+        echo "<script type=\"text/javascript\">history.pushState(null, '', '" . $newUrl . "');</script>";
+    }
 }
